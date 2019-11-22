8ef53fc5bab007fca30cc756b790273c8322d5ea
Jeremiah <02Jeremiah.Gonzales@gulllakecs.org>
2019-11-22 14:00:54 -0500

    Add fade program to LED

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -5,11 +5,15 @@
  * 
  * This program will turn on an LED for a specified amounts of time, turn it
  * off, and back on in a While Loop
+ * 
+ * Update: 11.22.19
+ * Program: Update blink to allow my LED to fade.
  */
 
-//initialize our led to Pin 13
-int led = 13;
-
+//initialize our led to Pin 9
+int led = 9;  //the pin that the LED is in on the Arduino board
+int brightness = 0; //how bright the LED is
+int fadeAmount = 5; //how many points to fade the LED
 
 void setup() {
   // Using the Funtion pinMode to call the varible led and provide Output
@@ -17,9 +21,16 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(led, HIGH);
-  delay(1500);
-  digitalWrite(led, LOW);
-  delay(500);
+  // set the brightnes of pin9 (LED)
+  analogWrite(led, brightness);
+
+  //change the brightness for the next time through the loop
+  brightness = brightness + fadeAmount;
+
+  //reverse the direction of the fading at the end of the fade
+  if (brightness == 0 || brightness == 255) {
+    fadeAmount = -fadeAmount;
+  }
+  delay(30);
+  
 }
