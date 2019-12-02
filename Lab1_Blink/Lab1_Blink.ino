7e2b488b0373c3ee50e83541a3e209da74dda9de
Jeremiah <02Jeremiah.Gonzales@gulllakecs.org>
2019-12-02 14:07:13 -0500

    merged the digital read and serial port with master

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -5,14 +5,24 @@
  * 
  * This program will turn on an LED for a specified amounts of time, turn it
  * off, and back on in a While Loop
+ *
+Merged 12/2/19
+ * Programmer: Jeremiah
+ * Date: 11.14.19
+ * Program: digitalRead() & the Serial Port
+ * 
+ * Reads a digital input on pin 2, prints the result to the serial monitor 
  */
 
 //initialize our led to Pin 13
 int led = 13;
 
+//digital pin2 has a push button attached to it
+int PushButton = 3;
 
 void setup() {
   // Using the Funtion pinMode to call the varible led and provide Output
+  Serial.begin(9600);
   pinMode(led, OUTPUT);
 }
 
@@ -22,4 +32,10 @@ void loop() {
   delay(1500);
   digitalWrite(led, LOW);
   delay(500);
+
+   // read the input pin
+ int buttonState = digitalRead(PushButton);
+ //print out the state of the button 
+ Serial.println(buttonState);
+ delay(250); //delay in between readings for stability
 }
