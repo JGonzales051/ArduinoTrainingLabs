333b85387650799db43c7f75253d07ca225bd053
Jeremiah <02Jeremiah.Gonzales@gulllakecs.org>
2019-12-03 14:18:30 -0500

    Working Potentiometer Program

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -9,5 +9,20 @@
 */
 
 void setup() {
+ //initialize the serial communication at 9600 bits per second
+ Serial.begin(9600);
+}
+
+
 
 void loop() {
+ // read the input on analog pin 0
+ int potentiometerValue = analogRead(A0);
+
+ //print the value of our potentiometer
+ Serial.println(potentiometerValue)
+
+ //add a delay for stabuility
+ ;delay(1);
+ 
+}
